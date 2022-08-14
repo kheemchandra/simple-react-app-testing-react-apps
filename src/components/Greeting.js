49bd6395af2c223608516bc7467270b69e4d4ade
Kheem Chandra <kheemchandrasingh@gmail.com>
2022-08-14 10:06:49 +0530

    Test if content changed after button was  clicked

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,7 +1,17 @@
+import { useState } from "react";
+
 const Greeting = () => {
+  const [textChanged, setTextChanged] = useState(false);
+
+  const textChangeHandler = () => {
+    setTextChanged(true);
+  }
+
   return <div>
     <h1>Hello World!</h1>
-    <p>It's nice to see you again!</p>
+    {!textChanged && <p>It's nice to see you again!</p>}
+    {textChanged && <p>Text changed!</p>}
+    <button onClick={textChangeHandler}>Change!</button>
   </div>
 };
 
--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from  "@testing-library/react"; 
-// import describe from '@testing-library/jest-dom';
+import userEvent from "@testing-library/user-event";
 
 import Greeting from "./Greeting";
 
@@ -14,5 +14,43 @@ describe('Greeting component!', () => {
     // Assert
     const helloWorldElement = screen.getByText('Hello World!');
     expect(helloWorldElement).toBeInTheDocument;
+  });
+
+  test('renders "nice to see" on screen BEFORE button was clicked', () => {
+    // Arrange
+    render(<Greeting />);
+
+    // Act
+    // ... nothing
+
+    // Assert
+    const outputElement = screen.getByText('nice to see', {exact: false});
+    expect(outputElement).toBeInTheDocument;
+  });
+
+  test('renders "Text changed!" on screen AFTER button was clicked', () => {
+    // Arrange
+    render(<Greeting />);
+
+    // Act 
+    const buttonElement = screen.getByRole('button');
+    userEvent.click(buttonElement);
+
+    // Assert
+    const outputElement = screen.getByText('Text changed', { exact: false});
+    expect(outputElement).toBeInTheDocument;
+  });
+
+  test('does NOT render "nice to see" on screen AFTER button was clicked', () => {
+    // Arrange
+    render(<Greeting />);
+
+    // Act
+    const buttonElement = screen.getByRole('button');
+    userEvent.click(buttonElement);
+
+    // Assert
+    const outputElement = screen.queryByText('nice to see', { exact: false});
+    expect(outputElement).toBeNull;
   });
 });
